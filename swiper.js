54fc81f76d20f58fe5745fa12b947cefbe48b4e7
Oluseyi Immanuel <oluseyimayotomiwa@outlook.com>
2024-07-26 03:54:10 -0700

    feat(core): added all images and dependencies as well as landing page code

---
+++ swiper.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    }
+});
--- toggleMobileMenu.js
+++ toggleMobileMenu.js
@@ -10,3 +10,4 @@ function toggleMobileMenu() {
         }, 300);
     }
 }
+
